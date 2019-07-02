feb1df511b47aeca98dbd551c0a2bc500f50eacc
zero1five <zerodengyin@gmail.com>
2019-07-02 17:32:05 +0800

    feat: add lru cache

--- src/model/basePage.js
+++ src/model/basePage.js
@@ -34,12 +34,36 @@ export default {
       html: ''
     }
   },
-  reducer: {},
+  reducer: {
+    setPage(state, { payload }) {
+      const { cache } = state
+      cache.set(payload.title, payload)
+
+      return { ...state, page: payload }
+    }
+  },
   effects: {
-    *fetchMarkdown({ call, put }, { payload }) {
+    *fetchMarkdown({ call, put, select }, { payload }) {
+      const cache = yield select(state => state.basePage.cache)
+      if (cache.has(payload)) {
+        yield put({
+          type: 'setPage',
+          payload: cache.get(payload)
+        })
+        return
+      }
+
       const fp = resolveFilePath(creatorConfig.navi, payload.replace('/', ''))
       const res = yield call(readFile('http://localhost:3001' + fp))
-      console.log(res)
+
+      yield put({
+        type: 'setPage',
+        payload: {
+          title: payload,
+          headings: null,
+          html: res
+        }
+      })
     }
   }
 }
