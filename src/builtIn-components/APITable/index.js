0a3fa6dffe8b8c1d83c8c1c068ef2cbb84a7f46f
zero1five <zerodengyin@gmail.com>
2019-07-21 05:31:48 +0800

    fix: path reoslve

--- src/builtIn-components/APITable/index.js
+++ src/builtIn-components/APITable/index.js
@@ -6,25 +6,29 @@ import './index.less'
 const columns = [
   {
     title: '属性',
-    key: 'attribute',
-    dataIndex: 'attr'
+    key: 'attr',
+    dataIndex: 'attr',
+    // className: 'api-attr',
+    render: (text, record, index) => <span className="api-attr">{text}</span>
   },
   {
     title: '说明',
-    key: 'description',
+    key: 'desc',
     dataIndex: 'desc'
   },
   {
     title: '类型',
     key: 'type',
     dataIndex: 'type',
-    className: 'api-title'
+    // className: 'api-type',
+    render: (text, record, index) => <span className="api-type">{text}</span>
   },
   {
     title: '默认值',
     key: 'defaults',
     dataIndex: 'defaults',
-    className: 'api-defaults'
+    className: 'api-defaults',
+    render: (text, record, index) => <code>{text}</code>
   },
   {
     title: '版本',
@@ -41,8 +45,20 @@ export default class APITable extends PureComponent {
 
   render() {
     const { columns, data } = this.props
+
     return (
-      <Table columns={columns} dataSource={data} className="api-container" />
+      <Table
+        columns={columns}
+        dataSource={data.map(x => {
+          for (let key in x) {
+            if (!x[key]) x[key] = ''
+          }
+          return x
+        })}
+        className="api-container"
+        rowKey="attr"
+        pagination={false}
+      />
     )
   }
 }
